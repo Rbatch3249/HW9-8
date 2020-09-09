d7ad82e8055454f334e6b5cdb592a1ed2e871ec6
Ryan <rbatchelor3249@gmail.com>
2020-09-09 08:05:39 -0400

    This is Java homework

---
+++ src/main/java/inClass.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class inClass {
+    public static int multiply(int a, int b, int c){//Question1
+
+        return 0;
+
+
+    }
+
+
+    public static void print(){ //Question 2
+        System.out.println("Hello");
+    }
+    public static int value(){//Question 3
+        return 23;
+    }
+    public static void oddNumbers(){//Question 4
+
+        for(int x = 13; x < 26; x++){
+            if(x % 2 != 0);
+        }
+
+    }
+    public static void theDifference(){
+        System.out.println(10 -3);
+    }
+    public static void timesThree(){
+        int[] array = new int[]{4, 15,9};
+        for(int x = 0; x < array.length; x++){
+
+        }
+
+    }
+    public static void main(String[] args) {
+
+    multiply(3,4,5);//Question1
+    print();//Question 2
+    value();//Question 3
+    oddNumbers();//Question 4
+    theDifference();//Question 5
+    timesThree();
+
+
+    }
+}
+
+
+
+
+
+
+
